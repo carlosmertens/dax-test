2b68bd4c8fe060702ba128c1647e9bc57e036cf4
Carlos Mertens <mertenscarlos@yahoo.com>
2020-09-04 00:23:48 +0200

    Updates

---
+++ precache-manifest.ffa6c9cdedb3fa0511c4896c028b7a3e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cebdbaef9db04bf85a4b2ed8545d0269",
+    "url": "/dax-test/index.html"
+  },
+  {
+    "revision": "7a6163bda33fe9b8eaf7",
+    "url": "/dax-test/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a8e3d719987cd9bd72c3",
+    "url": "/dax-test/static/css/main.4c407a4a.chunk.css"
+  },
+  {
+    "revision": "7a6163bda33fe9b8eaf7",
+    "url": "/dax-test/static/js/2.409fc41f.chunk.js"
+  },
+  {
+    "revision": "12dd44d2b00e40875f57730a1f749703",
+    "url": "/dax-test/static/js/2.409fc41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8e3d719987cd9bd72c3",
+    "url": "/dax-test/static/js/main.a0f2381c.chunk.js"
+  },
+  {
+    "revision": "c83800016a44b6c455da",
+    "url": "/dax-test/static/js/runtime-main.cda47ce6.js"
+  },
+  {
+    "revision": "2c8be56974317c162171beecb6138a8a",
+    "url": "/dax-test/static/media/Page-1.2c8be569.svg"
+  },
+  {
+    "revision": "80fad2b21408dc6af1092dd4d03d2ef7",
+    "url": "/dax-test/static/media/bulldozer.80fad2b2.svg"
+  },
+  {
+    "revision": "4419245db92d91b86347c8d9b40ac29c",
+    "url": "/dax-test/static/media/crane.4419245d.svg"
+  },
+  {
+    "revision": "f08a680122b18e24baa306f60e95f1f6",
+    "url": "/dax-test/static/media/harvester.f08a6801.svg"
+  },
+  {
+    "revision": "a643a28a2c2edc37d4158576b0466484",
+    "url": "/dax-test/static/media/logoCentro.a643a28a.png"
+  },
+  {
+    "revision": "2b39405ce66a7dc16e23162d7e329c39",
+    "url": "/dax-test/static/media/machine.2b39405c.svg"
+  },
+  {
+    "revision": "5edb7a82d311190d2dc559a051b8877d",
+    "url": "/dax-test/static/media/mapaDAX.5edb7a82.png"
+  },
+  {
+    "revision": "bd9a8e03a32d8edd46119af015308e66",
+    "url": "/dax-test/static/media/truck.bd9a8e03.svg"
+  }
+]);
